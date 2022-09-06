caea1705ec7e2ea436ed97017939c45845642f44
Dannick Hoorn <s1159965@student.windesheim.nl>
2022-09-06 15:50:52 +0200

    opgave 1

---
+++ Les 1/Week 1_Opgave/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a;
+	int b;
+	int c;
+	int antwoord;
+
+	a = 10;
+	b = 2;
+	c = 5;
+
+	antwoord = a / b * c; //vermenigvuldiging van a en b
+
+	cout << "Het antwoord is: " << antwoord << "\n"; //print het antwoord
+
+	return 0;
+}
