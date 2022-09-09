4feb25b4b1eec3f3f3058be9187a3e9c1a73c051
Dannick Hoorn <s1159965@student.windesheim.nl>
2022-09-09 19:25:50 +0200

    add random shit

---
+++ Week_2/Random Shit/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int count = 0;
+	while (count < 69421) {
+		std::cout << count << '\n';
+		count++;
+	}
+	std::cout << "nice";
+	return 0;
+}
