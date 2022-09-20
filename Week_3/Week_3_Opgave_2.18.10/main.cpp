1b9211e3e00d9e5c13987412bfcc9315a9197b6f
Dannick Hoorn <s1159965@student.windesheim.nl>
2022-09-20 15:53:35 +0200

    compacting 2.18.10, 2.18.8

--- Week_3/Week_3_Opgave_2.18.10/main.cpp
+++ Week_3/Week_3_Opgave_2.18.10/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main() {
 	double geld = 1000.00;
+	double factor = 1.005;
 
 	cout << setw(15) << "Jaar:";
 	cout << setw(15) << "Saldo:\n";
 
 	for (int jaar = 2022; jaar < 2033; jaar++) {
-		geld *= 1.005;
+		geld *= factor;
 		cout << setw(15) << jaar;
 		cout << setw(15) << geld << "\n";
 	}
--- Week_3/Week_3_Opgave_2.18.8/main.cpp
+++ Week_3/Week_3_Opgave_2.18.8/main.cpp
@@ -4,17 +4,11 @@
 using namespace std;
 
 int main() {
-	cout << setw(5) << "x";
-	cout << setw(12) << "kwadraat";
-	cout << setw(12) << "derde macht";
-	cout << "\n";
+	cout << setw(5) << "x" << setw(12) << "kwadraat" << setw(12) << "derde macht" << "\n";
 	
 	cout << std::setprecision(2) << showpoint << fixed; // zorgt voor notatie xxx.xx, xx.xx of x.xx
 	
 	for (float x = 1; x < 8; x += 0.50) {
-		cout << setw(5) << x;
-		cout << setw(12) << x * x;
-		cout << setw(12) << x * x * x;
-		cout << "\n";
+		cout << setw(5) << x << setw(12) << x * x << setw(12) << x * x * x << "\n";
 	}
 }
