d52f545b272ea55cf73a52ea50b04117d6cc4c2d
Dannick Hoorn <s1159965@student.windesheim.nl>
2022-10-04 15:36:09 +0200

    add 3.19.09

---
+++ Week_5/Week_5_Opdracht_3.19.09/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void fac(int n, long long& resultaat);
+
+int main() {
+	int input;
+	long long resultaat;
+
+	cin >> input;
+	cin.get();
+
+	fac(input, resultaat);
+
+	cout << resultaat;
+	return 0;
+}
+
+
+void fac(int n, long long& resultaat) {
+	int count = 0;
+	int a = 1;
+	if (n > 0 && n < 17) {
+		while (count != n) {
+			a *= (count + 1);
+			count++;
+		}
+		resultaat = a;
+	}
+	else if (n == 0) {
+		resultaat = 1;
+	}
+	else if (n < 0) {
+		resultaat = 0;
+	}
+	else {
+		resultaat = -1;
+		cout << "Te groot getal ingevoerd" << endl;
+	}
+}
+//je kunt tot 33 gaan.
