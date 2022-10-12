6617e39a28b640627b64b981f7e1b6d45ffe16cc
Dannick Hoorn <s1159965@student.windesheim.nl>
2022-10-12 12:25:13 +0200

    vanalles

---
+++ Week_6/Week_6_Opgave_4.20.06/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a[15] = { 10, 20, 30, 30, 30, 14, 14 ,14 ,14, 14, 14, 14, 14, 14, 14 };
+	int arrayLengte = sizeof(a) / sizeof(int);
+
+	for (int i; i < arrayLengte; i++) {
+
+	}
+	
+}
