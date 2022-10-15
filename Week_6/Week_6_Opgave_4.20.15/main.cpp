af6658dbea598e48bb0b574ac78ece4c2ef0e0a5
Dannick Hoorn <s1159965@student.windesheim.nl>
2022-10-15 13:55:47 +0200

    4.20.15 werkte en toen heb ik het verneukt

--- Week_6/Week_6_Opgave_4.20.15/main.cpp
+++ Week_6/Week_6_Opgave_4.20.15/main.cpp
@@ -1,7 +1,38 @@
 #include<iostream>
+#include"Functions.h"
 
 using namespace std;
 
 int main() {
-	 
+
+	const int MaxAantal = 10;
+	int a[MaxAantal] = { 4,8,2,3,5,17,7,99,3,12 };
+	const int lengte = sizeof(a) / sizeof(int);
+	int getal;
+	char antwoord;
+
+	printArray(a, lengte);
+	Nogmaals:
+	cout << "Welk getal moet verwijderd worden? \n";
+
+	cin >> getal;
+	cin.get();
+
+	verwijder(getal, a, lengte);
+
+	printArray(a, lengte);
+
+	cout << "Wilt u nog een getal verwijderen? (j/n)\n";
+
+	cin >> antwoord;
+	cin.get();
+
+	switch (antwoord) {
+	case 'j': case 'J':
+		goto Nogmaals;
+	case 'n': case 'N':
+		break;
+	}
+	
+
 }
